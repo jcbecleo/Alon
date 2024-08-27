9dcbd3f80ba277dcea8f5ba53574c32b8a2d7b8f
jcbecleo <ecleojosscharyborj@gmail.com>
2024-08-27 14:04:33 +0800

    [chore] modals

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -3,6 +3,8 @@
 import { useCallback, useEffect, useState } from "react";
 import {IoMdClose} from "react-icons/io"
 
+import Button from "../Button";
+
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
@@ -80,8 +82,18 @@ const Modal: React.FC<ModalProps> = ({
                         <IoMdClose size={18}/> 
                     </button>
                     <div className="text-lg font-semibold">
-                        Login Modal
+                        {title}
                     </div>
+                </div>
+                {/*Body*/}
+                <div className="relative p-6 flex-auto">
+                  {body}
+                </div>
+                {/*Footer*/}
+                <div className="flex flex-col gap-2 p-6">
+                  <div className="flex flex-row items-center gap-4 w-full">
+                    <Button small label="My Button"/>
+                  </div>
                 </div>
             </div>
          </div>
